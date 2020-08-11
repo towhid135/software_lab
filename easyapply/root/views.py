6a1d9e0e9213cbc1d8b884ca60e1113fd77ef717
Towhidul Islam <17101135@uap-bd.edu>
2020-08-12 01:20:53 +0600

    last update
    last update

--- easyapply/root/views.py
+++ easyapply/root/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from root import models
+from django.db.models import Subquery
 
-# Create your views here.
+def user(request):
+        if (request.method == 'POST'):
+                img1 = request.POST.get("img1")
+                field1 = request.POST.get("field1")
+                address1 = request.POST.get("address1")
+                phone1 = request.POST.get("phone1")
+                email1 = request.POST.get("email1")
+                achi1 = request.POST.get("achi1")
+                objective1 = request.POST.get("objective1")
+                exp1 = request.POST.get("exp1")
+                userobj = models.user(field = field1 , address = address1, phone = phone1, email = email1, image = img1, achievement = achi1, objective = objective1, exp = exp1)
+                # print("true")
+                userobj.save()
+                return render(request, "user.html")
+        return render(request,"user.html")
