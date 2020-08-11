b51564a53ef718c79fd15b597c8d266b3e0e87eb
Towhidul Islam <17101135@uap-bd.edu>
2020-08-11 11:37:51 +0600

    first commit

---
+++ easyapply/root/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.db.models import Subquery
+from datetime import datetime,date
+
+class user(models.Model):
+    userid = models.IntegerField(default=1,primary_key=True)
+    field = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=11)
+    email = models.EmailField(max_length=100)
+    image = models.ImageField(upload_to=None, null=True, blank=True)
+    achievement = models.TextField()
+    objective = models.TextField()
+    exp = models.TextField()
+
+class skill(models.Model):
+    soft = models.TextField()
+    technical = models.TextField()
+    organizational = models.TextField()
+    computer = models.TextField()
+    language = models.TextField()
+    userid = models.ForeignKey(user,on_delete=models.CASCADE)
