485fac6e1f9bb6ef92f702f146251a680eb8353e
Towhidul Islam <17101135@uap-bd.edu>
2020-08-12 02:48:55 +0600

    imageupdate
    image

--- easyapply/easyapply/settings.py
+++ easyapply/easyapply/settings.py
@@ -117,13 +117,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+"""STATIC_URL = '/static/'
 
-"""STATICFILES_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
-]"""
+]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+MEDIA_URL = '/media/' """
+
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
 
--- easyapply/easyapply/urls.py
+++ easyapply/easyapply/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from root import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('user/',views.user),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
