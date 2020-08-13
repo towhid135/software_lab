08ab4fc79c5b65cf95715120ee8ac14da66ab2eb
Towhidul Islam <17101135@uap-bd.edu>
2020-08-13 21:25:30 +0600

    Final update for last lab
    Final update for last lab

--- easyapply/root/models.py
+++ easyapply/root/models.py
@@ -4,6 +4,7 @@ from datetime import datetime,date
 
 class user(models.Model):
     userid = models.AutoField(primary_key=True,null=False,unique=True)
+    username = models.CharField(max_length=200)
     field = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     phone = models.CharField(max_length=11)
@@ -19,13 +20,83 @@ class skill(models.Model):
     organizational = models.TextField()
     computer = models.TextField()
     language = models.TextField()
-    userid = models.ForeignKey(user,on_delete=models.CASCADE)
+    userid = models.ForeignKey(user,null=True,on_delete=models.CASCADE)
 
 
 class employer(models.Model):
     e_id = models.AutoField(primary_key=True,null=False,unique=True)
     company_name = models.CharField(max_length=200)
     company_address=models.TextField()
+
+
+
+class PSC(models.Model):
+    psc_id=models.AutoField(primary_key=True,null=False,unique=True)
+    pschool=models.TextField()
+    p_GPA=models.CharField(max_length=4)
+    p_board=models.CharField(max_length=20)
+    p_year=models.CharField(max_length=4)
+    userid = models.ForeignKey(user, null=True ,on_delete=models.CASCADE)
+
+class JSC(models.Model):
+    jsc_id=models.AutoField(primary_key=True,null=False,unique=True)
+    jschool=models.TextField()
+    j_GPA=models.CharField(max_length=4)
+    j_board=models.CharField(max_length=20)
+    j_year=models.CharField(max_length=4)
+    userid = models.ForeignKey(user, null=True, on_delete=models.CASCADE)
+
+class SSC(models.Model):
+    ssc_id=models.AutoField(primary_key=True,null=False,unique=True)
+    sschool=models.TextField()
+    s_group = models.CharField(max_length=10)
+    s_GPA=models.CharField(max_length=4)
+    s_board=models.CharField(max_length=20)
+    s_year=models.CharField(max_length=4)
+    userid = models.ForeignKey(user, null=True, on_delete=models.CASCADE)
+
+class HSC(models.Model):
+    hsc_id=models.AutoField(primary_key=True,null=False,unique=True)
+    hschool=models.TextField()
+    h_group = models.CharField(max_length=10)
+    h_GPA=models.CharField(max_length=4)
+    h_board=models.CharField(max_length=20)
+    h_year=models.CharField(max_length=4)
+    userid = models.ForeignKey(user, null=True, on_delete=models.CASCADE)
+
+class Honours(models.Model):
+    ho_id=models.AutoField(primary_key=True,null=False,unique=True)
+    ho_university=models.TextField()
+    ho_program=models.CharField(max_length=4)
+    ho_CGPA=models.CharField(max_length=4)
+    ho_year=models.CharField(max_length=4)
+    userid = models.ForeignKey(user, null=True, on_delete=models.CASCADE)
+
+class Masters(models.Model):
+    m_id=models.AutoField(primary_key=True,null=False,unique=True)
+    m_university=models.TextField()
+    m_program=models.CharField(max_length=4)
+    m_CGPA=models.CharField(max_length=4)
+    m_year=models.CharField(max_length=4)
+    userid = models.ForeignKey(user, null=True, on_delete=models.CASCADE)
+
+class PHD(models.Model):
+    phd_id=models.AutoField(primary_key=True,null=False,unique=True)
+    uni=models.TextField()
+    field=models.TextField()
+    phd_year=models.CharField(max_length=4)
+    userid = models.ForeignKey(user, null=True, on_delete=models.CASCADE)
+
+class Academic(models.Model):
+    a_id = models.AutoField(primary_key=True, null=False, unique=True)
+    psc_id = models.ForeignKey(PSC, on_delete=models.CASCADE,null =True,blank=True)
+    jsc_id = models.ForeignKey(JSC, on_delete=models.CASCADE,null =True,blank=True)
+    ssc_id= models.ForeignKey(SSC, on_delete=models.CASCADE,null =True,blank=True)
+    hsc_id = models.ForeignKey(HSC, on_delete=models.CASCADE, null=True, blank=True)
+    ho_id  = models.ForeignKey(Honours, on_delete=models.CASCADE,null =True,blank=True)
+    m_id   = models.ForeignKey(Masters, on_delete=models.CASCADE,null =True,blank=True)
+    phd_id = models.ForeignKey(PHD, on_delete=models.CASCADE,null =True,blank=True)
+
 
 
 class circular(models.Model):
@@ -36,6 +107,3 @@ class circular(models.Model):
     salary = models.CharField(max_length=30)
     requirements = models.TextField()
     date = models.CharField(max_length=30)
-
-
-
--- easyapply/root/views.py
+++ easyapply/root/views.py
@@ -30,18 +30,35 @@ def user(request):
         if (request.method == 'POST'):
                 img1 = request.FILES.get("img1")
                 field1 = request.POST.get("field1")
+                username1 = request.POST.get("username1")
                 address1 = request.POST.get("address1")
                 phone1 = request.POST.get("phone1")
                 email1 = request.POST.get("email1")
                 achi1 = request.POST.get("achi1")
                 objective1 = request.POST.get("objective1")
                 exp1 = request.POST.get("exp1")
-                userobj = models.user(field = field1 , address = address1, phone = phone1, email = email1, image = img1, achievement = achi1, objective = objective1, exp = exp1)
+                userobj = models.user(field = field1 ,username=username1, address = address1, phone = phone1, email = email1, image = img1, achievement = achi1, objective = objective1, exp = exp1)
                 # print("true")
                 userobj.save()
                 return render(request, "user.html")
         return render(request,"user.html")
 
+@login_required
+def skill(request):
+    if (request.method == 'POST'):
+        soft1 = request.POST.get("soft1")
+        tech1 = request.POST.get("tech1")
+        org1 = request.POST.get("org1")
+        cp1 = request.POST.get("cp1")
+        lang1 = request.POST.get("lang1")
+
+        skillobj = models.skill(soft=soft1, technical=tech1, organizational=org1, computer=cp1, language=lang1)
+        # print("true")
+        skillobj.save()
+        return render(request, "skill.html")
+    return render(request, "skill.html")
+
+@login_required
 def circular(request):
         if (request.method == 'POST'):
                 title = request.POST.get("title")
@@ -54,7 +71,7 @@ def circular(request):
                 circularobj.save()
                 return render(request, "circular.html")
         return render(request,"circular.html")
-
+@login_required
 def employer(request):
         if (request.method == 'POST'):
                 name = request.POST.get("name")
@@ -65,3 +82,91 @@ def employer(request):
                 return render(request, "employer.html")
         return render(request,"employer.html")
 
+@login_required
+def addpsc(request):
+    if (request.method == 'POST'):
+        pscl = request.POST.get("sname")
+        gpa = request.POST.get("gpa")
+        board = request.POST.get("board")
+        year = request.POST.get("year")
+        pscinfo = models.PSC(pschool=pscl, p_GPA=gpa, p_board=board, p_year=year)
+        pscinfo.save()
+        return render(request, "addpsc.html")
+    return render(request, "addpsc.html")
+
+@login_required
+def addjsc(request):
+        if (request.method == 'POST'):
+                jscl = request.POST.get("jname")
+                gpa = request.POST.get("gpa")
+                board = request.POST.get("board")
+                year = request.POST.get("year")
+                jscinfo = models.JSC(jschool=jscl, j_GPA=gpa, j_board =board, j_year=year)
+                jscinfo.save()
+                return render(request, "addjsc.html")
+        return render(request, "addjsc.html")
+
+@login_required
+def addssc(request):
+        if (request.method == 'POST'):
+                scl = request.POST.get("name")
+                grp=  request.POST.get("grp")
+                gpa = request.POST.get("gpa")
+                board = request.POST.get("board")
+                year = request.POST.get("year")
+                info = models.SSC(sschool=scl, s_group=grp, s_GPA=gpa, s_board =board, s_year=year)
+                info.save()
+                return render(request, "addssc.html")
+        return render(request, "addssc.html")
+
+@login_required
+def addhsc(request):
+        if (request.method == 'POST'):
+                scl = request.POST.get("name")
+                grp=  request.POST.get("grp")
+                gpa = request.POST.get("gpa")
+                board = request.POST.get("board")
+                year = request.POST.get("year")
+                info = models.HSC(hschool=scl, h_group=grp,  h_GPA=gpa, h_board =board, h_year=year)
+                info.save()
+                return render(request, "addhsc.html")
+        return render(request, "addhsc.html")
+
+@login_required
+def addhonours(request):
+    if (request.method == 'POST'):
+        uni = request.POST.get("uni")
+        prgm = request.POST.get("prgm")
+        cgpa = request.POST.get("gpa")
+        year = request.POST.get("year")
+        info = models.Honours(ho_university=uni,ho_program=prgm, ho_CGPA=cgpa, ho_year=year)
+        info.save()
+        return render(request, "addhonours.html")
+    return render(request, "addhonours.html")
+
+@login_required
+def addmasters(request):
+    if (request.method == 'POST'):
+        uni = request.POST.get("name")
+        prgm = request.POST.get("prgm")
+        cgpa = request.POST.get("gpa")
+        year = request.POST.get("year")
+        info = models.Masters(m_university=uni,m_program=prgm, m_CGPA=cgpa, m_year=year)
+        info.save()
+        return render(request, "addmasters.html")
+    return render(request, "addmasters.html")
+@login_required
+def addphd(request):
+    if (request.method == 'POST'):
+        uni=request.POST.get("name")
+        field = request.POST.get("name")
+        year = request.POST.get("year")
+        info = models.PHD(uni=uni,field=field,phd_year=year)
+        info.save()
+        return render(request, "addphd.html")
+    return render(request, "addphd.html")
+
+
+@login_required
+def results(request):
+    return render (request,'results.html')
